c3c80dd87da5c4a691bc2f3db482aecdab318444
Facundo <facundo91.1401@gmail.com>
2023-02-08 15:32:15 -0300

    first commit

---
+++ Info-Trabajo-Final/Script.js
@@ -0,0 +1,54 @@
+// let play = document.querySelector('#play')
+// let pausa = document.querySelector('#pausa')
+let video = document.querySelector('#video')
+let tiempoVideo = document.querySelector('#tiempoVideo')
+
+// tiempoVideo.innerText = video.duration
+
+// console.log(tiempoVideo)
+
+document.addEventListener('click', (e) => {
+
+    const target = e.target
+
+    if (target.matches('#play')) {
+        video.play()
+
+        const duration = video.duration / 60
+        console.log(duration)
+        const minutes = Math.trunc(duration)
+        console.log(minutes)
+        const minutesOff = duration - minutes
+        console.log(minutesOff)
+        const seconds = Math.trunc(minutesOff * 60)
+        console.log(seconds)
+        tiempoVideo.innerText = `Duracion total del video: ${minutes}: ${seconds}`
+
+
+    }
+
+    if (target.matches('#play img')) {
+        video.play()
+
+        const duration = video.duration / 60
+        console.log(duration)
+        const minutes = Math.trunc(duration)
+        console.log(minutes)
+        const minutesOff = duration - minutes
+        console.log(minutesOff)
+        const seconds = Math.trunc(minutesOff * 60)
+        console.log(seconds)
+        tiempoVideo.innerText = `Duracion total del video: ${minutes}: ${seconds}`
+    }
+
+    if (target.matches('#pausa')) {
+        // alert("hiciste pausa")
+        video.pause()
+    }
+
+    if (target.matches('#pausa img')) {
+        video.pause()
+    }
+
+})
+
